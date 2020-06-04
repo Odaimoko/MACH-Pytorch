0998d828dbeb64e6eb231df9a4bf3bb899fccb71
oda <Odaimoko@gmail.com>
2020-06-04 19:43:24 +0100

    training wiki10 script

--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 for ((i = 0; i < 8; i++)); do
     for ((j = 0; j < 4; j++)); do
-        part=$(($i*4+$j))
+        part=$(($i * 4 + $j))
         echo $part
-         python -W ignore::Warning src/train.py --rep $part --model config/model/test.yaml \
-            --dataset config/dataset/test.yaml --gpu 0 &
+        export CUDA_VISIBLE_DEVICES=$j
+        python -W ignore::Warning src/train.py --rep $part --model config/model/wiki10.yaml \
+            --dataset config/dataset/wiki10.yaml --gpus $j &
     done
     wait
 
