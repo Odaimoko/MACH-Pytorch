43d60ca9da6b5c006c69363b1e8bcbf55263f373
oda <Odaimoko@gmail.com>
2020-06-10 17:12:01 +0100

    ready to trim delicious labels

--- train_eval_trim.sh
+++ train_eval_trim.sh
@@ -2,7 +2,7 @@
 #model=bibtex
 #data=Bibtex
 #type=rank
-while getopts 'm:d:' c; do
+while getopts 'm:d:t:' c; do
     case $c in
     m) model=$OPTARG ;;
     d) data=$OPTARG ;;
@@ -12,6 +12,6 @@ done
 
 for (( j = 1; j < 10; ++j)); do
     MODEL=$model
-    DATA=${data}_trimrank0.$j
+    DATA=${data}_trim${type}0.$j
     ./train.sh -m $MODEL -d $DATA
 done
