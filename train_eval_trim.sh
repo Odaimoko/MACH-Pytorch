d70348c73876ecefb4d2cfde4b1b9eb4ea8b68a7
oda <Odaimoko@gmail.com>
2020-06-10 16:32:49 +0100

    add argparse

--- train_eval_trim.sh
+++ train_eval_trim.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-model=bibtex
-data=Bibtex
-type=rank
+#model=bibtex
+#data=Bibtex
+#type=rank
+while getopts 'm:d:' c; do
+    case $c in
+    m) model=$OPTARG ;;
+    d) data=$OPTARG ;;
+    t) type=$OPTARG ;;
+    esac
+done
+
 for (( j = 1; j < 10; ++j)); do
     MODEL=$model
     DATA=${data}_trimrank0.$j
